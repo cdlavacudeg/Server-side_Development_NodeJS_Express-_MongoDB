e1d0a5c8cc6694165064b8566ed71bec4eb2da25
crislav10 <cristianlav10@gmail.com>
2021-12-04 16:20:09 -0500

    Assigment 3

--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const bodyParser=require('body-parser');
 const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
 
 const Promo = require('../models/promotions');
 
@@ -19,7 +20,7 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdminUser,(req, res, next) => {
     Promo.create(req.body)
     .then((promo) => {
         console.log('Promo Created ', promo);
@@ -29,11 +30,11 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdminUser,(req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdminUser,(req, res, next) => {
     Promo.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -53,11 +54,11 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdminUser,(req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdminUser,(req, res, next) => {
     Promo.findByIdAndUpdate(req.params.promoId, {
         $set: req.body
     }, { new: true })
@@ -68,7 +69,7 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdminUser,(req, res, next) => {
     Promo.findByIdAndRemove(req.params.promoId)
     .then((resp) => {
         res.statusCode = 200;
